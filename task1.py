dfc8201aed321bc9afb8ace99396a3bd171ee7fa
WillisJackson <78382106+Jacksonwillis005@users.noreply.github.com>
2021-04-19 09:29:11 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -6,3 +6,30 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+#!python3
+
+A1=str
+A2=str
+A3=str
+A4=str
+A5=str
+A6=str
+A7=str
+A8=str
+A9=str
+
+
+A1=input("Enter the first Adjective: ")
+A2=input("Enter Class in School: ")
+A3=input("Enter the Second Adjective: ")
+A4=input("Enter the Third Adjective: ")
+A5=input("Enter Room in the House: ")
+A6=input("Enter a Liquid: ")
+A7=input("Enter a Piece of Furniture: ")
+A8=input("Enter the Fourth Adjective: ")
+A9=input("Enter a Animal: ")
+
+print("On a Cold And " + A1 + " Day, I was walking Home after " + A2 + ". Then I Saw the " + A3 + " prints on the fornt steps of the " + A4 + " House. ")
+print("The door is open, and the " + A5 + " was covered in " + A6 + " prints.")
+print("Looking around, I heared a noise coming from behind the " + A7 + ".")
+print("Suddenly out of nowhere jump my " + A8 + " " + A9 + ".")
